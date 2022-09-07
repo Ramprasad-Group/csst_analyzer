1e33b9bddeb74771c6e4d2b0d0688d55aa417fd4
Mona Amrihesari <mamrihesari3@gatech.edu>
2022-09-07 12:05:28 -0400

    add test initialization

--- tests/test_csst_analyzer.py
+++ tests/test_csst_analyzer.py
@@ -1,4 +1,5 @@
 import pytest
+from pathlib import Path
 
 from csst import csst_analyzer
 from csst.csst_analyzer import CSSTA
@@ -6,3 +7,8 @@ from csst.csst_analyzer import CSSTA
 def test_version():
    assert csst_analyzer.__version__ == '0.1.0' 
 
+@pytest.fixture
+
+def test_CSSTA_init():
+    Path( 
+    CSSTA
