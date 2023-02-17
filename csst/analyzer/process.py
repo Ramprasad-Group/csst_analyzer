3a052a007604754d0d6283adfde00ae4597b42f9
jdkern11 <leolion29@gmail.com>
2023-02-17 15:28:08 -0500

    code analyzer to process

---
+++ csst/analyzer/process.py
@@ -0,0 +1,73 @@
+import logging
+from typing import Union, List
+import numpy as np
+
+from csst.analyzer.models import ProcessedTransmission
+from csst.experiment.models import Reactor
+
+logger = logging.getLogger(__name__)
+
+
+def reactor_transmission_at_temps(
+    reactor: Reactor,
+    temps: List[float],
+    temp_range: float = 0,
+) -> List[ProcessedTransmission]:
+    """Process the transmission values of the reactor at passed temps.
+
+    Args:
+        reactor: reactor to process
+        temps: temperatures to process
+        temp_range: the range of temperatures the transmission is processed from
+            (e.g., average_temperature +- (temperature_range / 2)) non-inclusive of
+            the upper value.
+    """
+    transmissions = []
+    for temp in temps:
+        ptrans = reactor_transmission_at_temp(reactor, temp, temp_range)
+        if ptrans is not None:
+            transmissions.append(ptrans)
+    return transmissions
+
+
+def reactor_transmission_at_temp(
+    reactor: Reactor, temp: float, temp_range: float = 0
+) -> Union[None, ProcessedTransmission]:
+    """Returns the average transmission at temperature for the reactor
+
+    Args:
+        reactor: reactor to process
+        temp: temperature to process at
+        temp_range: the range of temperatures the transmission is processed from
+            (e.g., average_temperature +- (temperature_range / 2)) non-inclusive of
+            the upper value.
+
+    Returns:
+        Process transmission value or None
+    """
+    half_range = temp_range / 2
+    # where returns a tuple but since this is a 1d array, the tuple has one element
+    # that is the list of indices
+    if temp_range == 0:
+        temp_indices = np.where(reactor.actual_temperature.values == temp)[0]
+    else:
+        temp_indices = np.where(
+            (
+                (reactor.actual_temperature.values < temp + half_range)
+                & (reactor.actual_temperature.values >= temp - half_range)
+            )
+        )[0]
+    if len(temp_indices) == 0:
+        logger.debug(
+            f"No index found at temperature {temp} +/- " + f"{round(temp_range / 2, 2)}"
+        )
+        return None
+    transmission = reactor.transmission.values[temp_indices]
+    return ProcessedTransmission(
+        reactor=reactor,
+        average_temperature=temp,
+        temperature_range=temp_range,
+        average=transmission.mean(),
+        median=np.median(transmission),
+        std=transmission.std(),
+    )
--- tests/test_analyzer_process.py
+++ tests/test_analyzer_process.py
@@ -1,48 +1,47 @@
-"""Tests csst.analyzer.models.AverageTransmission"""
 import pytest
 
-from csst.analyzer import (
-    process_reactor_transmission_at_temp,
-    process_reactor_transmission_at_temps,
+from csst.analyzer.process import (
+    reactor_transmission_at_temp,
+    reactor_transmission_at_temps,
 )
 from csst.experiment.models import PropertyValues, PropertyValue
 from .fixtures.data import reactor
 
 
-def test_process_reactor_transmission_at_temp(reactor):
+def test_reactor_transmission_at_temp(reactor):
     """Tests processing of transmission. See reactor fixture for data"""
-    ptrans = process_reactor_transmission_at_temp(reactor, temp=5)
+    ptrans = reactor_transmission_at_temp(reactor, temp=5)
     assert ptrans.average == 4.5
     assert ptrans.median == 4.5
     assert ptrans.reactor == reactor
     assert round(ptrans.std, 3) == 0.5
 
-    ptrans = process_reactor_transmission_at_temp(reactor, temp=5, temp_range=10)
+    ptrans = reactor_transmission_at_temp(reactor, temp=5, temp_range=10)
     assert ptrans.average == 4.5
     assert ptrans.median == 4.5
     assert ptrans.reactor == reactor
     assert round(ptrans.std, 3) == 0.5
 
-    ptrans = process_reactor_transmission_at_temp(reactor, temp=5, temp_range=11)
+    ptrans = reactor_transmission_at_temp(reactor, temp=5, temp_range=11)
     assert ptrans.average == 12.75
     assert ptrans.median == 12.5
     assert ptrans.reactor == reactor
     assert round(ptrans.std, 3) == 8.288
 
-    ptrans = process_reactor_transmission_at_temp(reactor, temp=15, temp_range=11)
+    ptrans = reactor_transmission_at_temp(reactor, temp=15, temp_range=11)
     assert ptrans.average == 56.5
     assert ptrans.median == 64
     assert ptrans.reactor == reactor
     assert round(ptrans.std, 3) == 24.187
 
-    assert process_reactor_transmission_at_temp(reactor, temp=0) is None
+    assert reactor_transmission_at_temp(reactor, temp=0) is None
 
 
-def test_process_reactor_transmission_at_temps(reactor):
+def test_reactor_transmission_at_temps(reactor):
     # reactor values sorted by temp would be
     # temp = [5, 5, 10, 10, 15, 15, 20, 20, 20, 20]
     # trans = [5, 4, 20, 22, 50, 45, 78, 78, 79, 80]
-    ptransmissions = process_reactor_transmission_at_temps(reactor, [5, 10, 15, 20])
+    ptransmissions = reactor_transmission_at_temps(reactor, [5, 10, 15, 20])
     averages = [ptrans.average for ptrans in ptransmissions]
     temps = [ptrans.average_temperature for ptrans in ptransmissions]
     averages.sort()
@@ -50,7 +49,7 @@ def test_process_reactor_transmission_at_temps(reactor):
     assert averages == expected_averages
     assert temps == [5, 10, 15, 20]
 
-    ptransmissions = process_reactor_transmission_at_temps(
+    ptransmissions = reactor_transmission_at_temps(
         reactor, [6, 11, 16, 19, 21], 2
     )
     averages = [ptrans.average for ptrans in ptransmissions]
