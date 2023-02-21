c85073b9da380e6924cc28f820143921e7017b72
jdkern11 <leolion29@gmail.com>
2023-02-21 11:43:46 -0500

    add additional datetime format

--- csst/experiment/helpers.py
+++ csst/experiment/helpers.py
@@ -5,7 +5,7 @@ def try_parsing_date(text):
     """Parse multiple date types or raise ValueError"""
     # remove excess whitespace between words
     # text = " ".join(text.split())
-    for fmt in ["%m/%d/%Y %I:%M:%S %p", "%m/%d/%y %H:%M"]:
+    for fmt in ["%m/%d/%Y %I:%M:%S %p", "%m/%d/%y %H:%M", "%Y-%m-%d %H:%M:%S"]:
         try:
             return datetime.strptime(text, fmt)
         except ValueError:
