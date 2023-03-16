e0f8b8125506d7189721e47186a03652f06f5307
jdkern11 <leolion29@gmail.com>
2023-03-16 14:15:36 -0400

    add prettier printing for temperature program and values

--- csst/experiment/models.py
+++ csst/experiment/models.py
@@ -34,7 +34,7 @@ class PropertyValue(BaseModel):
 
     def __str__(self):
         """String representation is the name, value and unit"""
-        return f"{self.name.value} is {self.value} {self.unit}"
+        return f"{' '.join(self.name.value.capitalize().split('_'))} is {self.value} {self.unit}"
 
 
 class PropertyValues(BaseModel):
@@ -57,7 +57,7 @@ class PropertyValues(BaseModel):
 
     def __str__(self):
         """String representation is just the name (unit) since values would be long"""
-        return f"{self.name} ({self.unit})"
+        return f"Storing {' '.join(self.name.value.split('_'))} in {self.unit} for {len(self.values)} datapoints"
 
 
 class TemperatureSettingEnum(str, Enum):
@@ -78,6 +78,9 @@ class TemperatureChange(BaseModel):
     to: PropertyValue
     rate: PropertyValue
 
+    def __str__(self):
+        return f"{self.setting.value.capitalize()} sample {self.to.name} to {self.to.value} {self.to.unit} at rate of {self.rate.value} {self.rate.unit}"
+
 
 class TemperatureHold(BaseModel):
     """Temperature hold model for temperature program
@@ -89,6 +92,9 @@ class TemperatureHold(BaseModel):
 
     at: PropertyValue
     for_: PropertyValue
+
+    def __str__(self) -> str:
+        return f"Hold sample at a {self.at.name.value} of {self.at.value} {self.at.unit} for a {self.for_.name.value} of {self.for_.value} {self.for_.unit}"
 
 
 class TemperatureProgram(BaseModel):
@@ -109,6 +115,19 @@ class TemperatureProgram(BaseModel):
     def hash(self) -> str:
         """Generates deterministic hash in hex format of the temperature program"""
         return hashlib.md5(json_dumps(self.json()).encode("utf-8")).digest().hex()
+
+    def __str__(self) -> str:
+        s = f"Block: {self.block}\n\nTuning stage:\n"
+        for step in self.solvent_tune:
+            s += str(step) + "\n"
+        s += "\nSample load stage:\n"
+        for step in self.sample_load:
+            s += str(step) + "\n"
+        s += "\nExperiment stage:\n"
+        for step in self.experiment:
+            s += str(step) + "\n"
+        s = s.strip("\n")
+        return s
 
 
 class Reactor(BaseModel):
